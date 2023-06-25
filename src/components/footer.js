3db4f9823a91d8e5284cf9c2658a989b45a4deda
BigCircle <zhuceafter2015@163.com>
2023-06-25 03:33:21 -0400

    almost good except 3 sliding images

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,10 +6,14 @@ class Footer extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <footer>
-            <hr>
-            <p>Created by <a href="https://dayuantan.github.io/AboutMe/">Dayuan Tan</a> and <a href="https://www.deer.ma/">Deer Ma</a> with love.<br>
-            Leave me a message by<a href="a form"> filling in this form.</a></p>
+            
+            <p>Created by <a href="https://dayuantan.github.io/AboutMe/">Dayuan Tan</a> and <a href="https://www.deer.ma/">Deer Ma</a> with love.
             <br>
+            Leave me a message by <a href="https://xh2uqicvmvy.typeform.com/to/jo6WmwDq">filling in this form</a>.
+            <br>
+            <br>
+            <div class="footer_copyright">Copyright &copy 2023 D Future Exploration LLC. All rights reserved. </div>
+            </p>
         </footer>
       `;
     }
